57b3800d447425a8c48e91403129360b7d7706c3
Saku0512 <moonandstar118@gmail.com>
2025-10-04 12:50:05 +0900

    feat: ホワイトリスト管理ページ追加

--- frontapp/src/lib/components/Sidebar.svelte
+++ frontapp/src/lib/components/Sidebar.svelte
@@ -35,6 +35,9 @@
         <a href="/dashboard/root/tournament-management" class="flex items-center px-4 py-2 text-sm font-medium rounded-md hover:bg-gray-700">
           トーナメント生成・管理
         </a>
+        <a href="/dashboard/root/whitelist-management" class="flex items-center px-4 py-2 text-sm font-medium rounded-md hover:bg-gray-700">
+          ホワイトリスト
+        </a>
       </div>
     {/if}
     
