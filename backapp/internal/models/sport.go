e538e1c6811d157e5742cf8f8162bb99ceeba433
Saku0512 <moonandstar118@gmail.com>
2025-10-07 09:45:11 +0900

    feat: 競技登録・管理機能を実装

---
+++ backapp/internal/models/sport.go
@@ -0,0 +1,16 @@
+package models
+
+// Sport represents the sports table
+type Sport struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+// EventSport represents the event_sports table
+type EventSport struct {
+	EventID     int    `json:"event_id"`
+	SportID     int    `json:"sport_id"`
+	Description string `json:"description"`
+	Rules       string `json:"rules"`
+	Location    string `json:"location"`
+}
