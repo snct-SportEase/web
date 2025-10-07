8c23bf1eddbf7519ae0ea0469d1e38e93895444a
Saku0512 <moonandstar118@gmail.com>
2025-10-07 09:57:56 +0900

    ref: サイドバーを上下スライド可能にした

--- frontapp/src/lib/components/Sidebar.svelte
+++ frontapp/src/lib/components/Sidebar.svelte
@@ -24,7 +24,7 @@
       <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
     </button>
   </div>
-  <nav class="flex-1 px-2 py-4 space-y-1">
+  <nav class="flex-1 px-2 py-4 space-y-1 overflow-y-auto">
     <!-- Root Menu -->
     {#if isRoot}
       <div class="pt-4">
